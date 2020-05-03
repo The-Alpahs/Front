ce3adab4da1b4b1c90e9f370e90c152e6c860f79
Yasindu Fernando <fernandoyasindo@gmail.com>
2020-05-02 22:15:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router,Switch,Route, Link} from "react-router-dom";
+import './App.css';
 
 import Login from './components/login.component';
 import Signup from './components/signup.component';
